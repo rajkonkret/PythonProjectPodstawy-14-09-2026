b127ac7f81f218538f360f21865f37316079971f
radek <rajkonkret660@gmail.com>
2026-09-14 15:24:51 +0200

    [1 26] update typy_danych_1.py.py

--- typy_danych_1.py
+++ typy_danych_1.py
@@ -1,3 +1,5 @@
+import sys
+
 wiek = 47  # int
 rok = 2026  # int
 temp = 36.6  # float
@@ -27,3 +29,11 @@ print(0.2 + 0.7)  # 0.8999999999999999
 # For example, in a floating-point arithmetic with five base-ten digits,
 # the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345
 # decimal - pozwala bezpiecznie pracowac z zaokrągleniami
+
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308,
+# max_exp=1024, max_10_exp=308,
+# min=2.2250738585072014e-308,
+# min_exp=-1021, min_10_exp=-307,
+# dig=15, mant_dig=53, epsilon=2.220446049250313e-16,
+# radix=2, rounds=1)
