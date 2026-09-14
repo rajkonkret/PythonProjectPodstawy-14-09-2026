ed21486752e45c38bff9fab7e90efd0423bfb917
radek <rajkonkret660@gmail.com>
2026-09-14 12:39:30 +0200

    [1 3] update pierwszy.py

--- pierwszy.py
+++ pierwszy.py
@@ -7,3 +7,20 @@ print("Hello World")
 # Hello World
 #
 # Hello World
+
+print("Nazywam się Radek")
+
+# ctrl / - komentarz
+# print('Radek")
+
+#   File "C:\Users\CSComarch\PycharmProjects\PythonProjectPodstawy-14-09-2026\pierwszy.py", line 13
+#     print('Radek")
+#           ^
+# SyntaxError: unterminated string literal (detected at line 13)
+#
+# Process finished with exit code 1
+
+print("Dalszy ciąg programu")
+# ctrl alt l - formatowanie kodu
+
+
