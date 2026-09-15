5ec049a714ef7de80044f8bfaaad636afb875ab1
radek <rajkonkret660@gmail.com>
2026-09-15 12:35:43 +0200

    [2 23] update typy_danych_4

--- dzien2/typy_danych_4_zbior.py
+++ dzien2/typy_danych_4_zbior.py
@@ -39,3 +39,16 @@ print(zbior)  # {66, 777, 11, 44, 18, 22, 54, 25}
 
 # operacje na zbiorach
 zbior_2 = {667, 11, 14, 44, 12.34, 18, 52, 667, 62, 99}
+
+# suma zbiorów - tworzy nowy zbior
+print(zbior | zbior_2)  # {66, 99, 777, 11, 44, 12.34, 14, 18, 52, 22, 54, 25, 667, 62}
+print(zbior.union(zbior_2))  # {66, 99, 777, 11, 44, 12.34, 14, 18, 52, 22, 54, 25, 667, 62}
+
+# częśc wspólna
+print(zbior & zbior_2)  # {18, 11, 44}
+print(zbior.intersection(zbior_2))  # {18, 11, 44}
+
+# różnica
+print(zbior - zbior_2)  # {66, 777, 54, 22, 25}
+print(zbior.difference(zbior_2))  # {66, 777, 54, 22, 25}
+print(zbior_2.difference(zbior))  # {99, 12.34, 14, 52, 667, 62}
