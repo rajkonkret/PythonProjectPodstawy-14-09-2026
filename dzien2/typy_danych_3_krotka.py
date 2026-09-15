6190b782dd9afc615c3ae8258836eb5e5ca52bea
radek <rajkonkret660@gmail.com>
2026-09-15 11:51:19 +0200

    [2 18] update typy_danych_3_krotka.py

---
+++ dzien2/typy_danych_3_krotka.py
@@ -0,0 +1,34 @@
+# krotka - kolekcja niemutowalna (tylko do odczytu)
+# pozwala efektywniej zarzadzać pamięcią
+
+tupla_imiona = "Zenek", "Tomek", "Marek", "Ania"
+print(type(tupla_imiona))
+print(tupla_imiona)  # <class 'tuple'>
+# ('Zenek', 'Tomek', 'Marek', 'Ania')
+
+# tupla_liczby = 43, 45, 22.34, 11, 200
+tupla_liczby = (43, 45, 22.34, 11, 200)
+print(type(tupla_liczby))  # <class 'tuple'>
+print(tupla_liczby)  # (43, 45, 22.34, 11, 200)
+
+# tupla jednoelementowa
+tupla_jeden = (45,)  # pep8 zaleca nawias dla tupli jednoelemntowych
+print(type(tupla_jeden))  # <class 'tuple'>
+
+tupla_dwa = tuple('45')
+print(tupla_dwa)
+# <class 'tuple'>
+# ('4', '5')
+
+# tupla_jeden[0] = 123
+# TypeError: 'tuple' object does not support item assignment
+del tupla_jeden
+# print(tupla_jeden) # NameError: name 'tupla_jeden' is not defined
+
+print(tupla_imiona)
+print(tupla_imiona.index("Zenek"))
+print(tupla_imiona.count("Zenek"))
+
+print(len(tupla_imiona))  # długosc 4
+
+
