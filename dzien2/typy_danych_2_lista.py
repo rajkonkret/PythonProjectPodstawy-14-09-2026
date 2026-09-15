198e2c04fc49744873f2f01e9eaf7b9fbc190fe3
radek <rajkonkret660@gmail.com>
2026-09-15 09:49:24 +0200

    [2 6] update typy_danych_2_lista.py

--- dzien2/typy_danych_2_lista.py
+++ dzien2/typy_danych_2_lista.py
@@ -61,3 +61,15 @@ print(lista[:])  # ['Radek', 'Tomek', 'Zenek', 'Grzegorz', 'Anna', 'Karolina']
 #     -6       -5       -4        -3         -2       -1
 print(lista[-2:0])  # [] -> [4:0]
 print(lista[0:-2])  # ['Radek', 'Tomek', 'Zenek', 'Grzegorz']
+
+# od 0 do 14
+lista_15 = list(range(15))  # 15 elementów od 0 do 14
+print(lista_15)  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+print(lista_15[::2])  # [start:stop:krok], [0, 2, 4, 6, 8, 10, 12, 14]
+print(lista_15[::3])  # [0, 3, 6, 9, 12]
+
+print(lista_15[::-1])
+# [14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+print(lista[-2:0:-1])  # ['Anna', 'Grzegorz', 'Zenek', 'Tomek']
