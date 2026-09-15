2217b4a89cf998d63252a8b376a64b2defea6d3e
radek <rajkonkret660@gmail.com>
2026-09-15 15:09:18 +0200

    [2 35] update warunki.py

---
+++ dzien2/warunki.py
@@ -0,0 +1,26 @@
+# instrukcje waarunkowe
+# instrukcje sterowania przepływem programu
+# if
+# w zależności od warunku wykona jeden lub drugi blok programu
+
+odp = True
+
+if odp: print("Test")
+
+if odp:
+    # blok programu wykonany gdy warunek True
+    print("Test")
+
+odp = False
+
+if odp:
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+    print("Brawo")
+
+print("Dalsza część programu")
