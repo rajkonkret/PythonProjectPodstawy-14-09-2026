f87c8bcd3fe2e0aa40118e6d070fc9615b70f0d0
radek <rajkonkret660@gmail.com>
2026-09-15 15:15:22 +0200

    [2 36] update warunki.py

--- dzien2/warunki.py
+++ dzien2/warunki.py
@@ -11,7 +11,9 @@ if odp:
     # blok programu wykonany gdy warunek True
     print("Test")
 
-odp = False
+# debugger - pozwala wykonać program krok po kroku
+# pułapki - miejsce gdzie program się zatrzyma
+odp = True
 
 if odp:
     print("Brawo")
