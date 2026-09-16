bce6178b904a329c4f055a381e8f2f5e61fb21ab
radek <rajkonkret660@gmail.com>
2026-09-16 12:27:05 +0200

    [3 23] update kl1.py

---
+++ dzien3/kl1.py
@@ -0,0 +1,33 @@
+# klasa - przepis, template
+# obiekt - zbudowany wg klasy
+# hermetyzacja, dziedziczenie, polimorfizm, abstrakcja
+
+# CamelCase
+class Human:
+    """
+    Klasa Human
+    """
+
+    def __init__(self, imie, wiek, plec="k"):
+        """
+        Metoda inicjalizujące
+        :param imie:
+        :param wiek:
+        :param plec:
+        """
+        self.imie = imie
+        self.wiek = wiek
+        self.plec = plec
+
+    def wypisz_wiek(self):
+        print(f"Mam na imię: {self.imie}")
+
+
+print(Human.__doc__)
+# Klasa Human
+cz1 = Human("Radek", 60, "m")
+print(cz1.imie)  # Radek
+print(cz1.wiek)  # 60
+print(cz1.plec)  # m
+
+cz1.wypisz_wiek() # Mam na imię: Radek
