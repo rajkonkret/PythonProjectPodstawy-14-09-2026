738a82adadbefcd48b6f032edf5af06c2bd960e0
radek <rajkonkret660@gmail.com>
2026-09-16 14:27:06 +0200

    [3 30] update csv_zad1.py

--- dzien3/csv_zad1.py
+++ dzien3/csv_zad1.py
@@ -5,10 +5,22 @@
 import csv
 
 row = ['radek', 'coe', "3", 0]
+fields = ['name', 'branch', "year", 'cgpa']
 
 filename = 'records.csv'
 
-with open(filename, "w") as csv_f:
+# newline="" - ominiecie problemu pustych na linii
+with open(filename, "w", newline="") as csv_f:
     csv_writer = csv.writer(csv_f)
+    csv_writer.writerow(fields)
     csv_writer.writerow(row)
 
+dict_name = dict(zip(fields, row))
+print(dict_name)
+# {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0}
+
+filename = 'records_dict.csv'
+with open(filename, "w", newline="") as csv_f:
+    csv_writer = csv.DictWriter(csv_f, fieldnames=fields)
+    csv_writer.writeheader()
+    csv_writer.writerow(dict_name)
