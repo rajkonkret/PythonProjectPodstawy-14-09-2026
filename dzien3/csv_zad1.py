0d04292a920bee150b92cc3870e4428baedf7ecd
radek <rajkonkret660@gmail.com>
2026-09-16 14:31:57 +0200

    [3 31] update csv_zad1.py

--- dzien3/csv_zad1.py
+++ dzien3/csv_zad1.py
@@ -24,3 +24,20 @@ with open(filename, "w", newline="") as csv_f:
     csv_writer = csv.DictWriter(csv_f, fieldnames=fields)
     csv_writer.writeheader()
     csv_writer.writerow(dict_name)
+
+lista = [
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0},
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0},
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0},
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0},
+    {'name': 'radek', 'branch': 'coe', 'year': '3', 'cgpa': 0},
+]
+
+filename = 'records_dict_list.csv'
+list_product = [key for key in lista[0]]
+
+with open(filename, "w", newline="") as csv_f:
+    # csv_writer = csv.DictWriter(csv_f, fieldnames=lista[0])
+    csv_writer = csv.DictWriter(csv_f, fieldnames=list_product, delimiter=";")
+    csv_writer.writeheader()
+    csv_writer.writerows(lista)  # writerows - lista
