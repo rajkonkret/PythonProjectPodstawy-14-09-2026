129bd27c53ffe809b9310b9d2f645b99a520a59d
radek <rajkonkret660@gmail.com>
2026-09-16 10:44:31 +0200

    [3 14] update petle2.py

---
+++ dzien3/petle_zad3.py
@@ -0,0 +1,20 @@
+# while - pętla sterowana warunkiem
+
+# pętla nieskończona
+# while True:
+#     print("Komunikat")
+#
+licznik  = 0
+
+while True:
+    licznik += 1 # licznik = licznik + 1
+    print("Komunikat 2 !!")
+    if licznik > 10:
+        break # przerywanie pętli
+
+print(licznik) # 11
+
+licznik = 0
+while  licznik < 10:
+    licznik += 1
+    print("Komunikat 3")
