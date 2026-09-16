8844488d5f6805e042888519d268f326a5c0bc6f
radek <rajkonkret660@gmail.com>
2026-09-16 09:56:30 +0200

    [3 4] update petle.py

--- dzien3/petle.py
+++ dzien3/petle.py
@@ -28,3 +28,15 @@ for i in range(10):
 # 6 parzysta
 # 8 parzysta
 
+lista3 = []
+# parzyste dodac do listy
+for i in range(10):
+    if i % 2 == 0:
+        lista3.append(i)
+
+print(lista3)  # [0, 2, 4, 6, 8]
+
+# list comprehensions
+lista3 = [j for j in range(10) if j % 2 == 0]
+print(lista3)  # [0, 2, 4, 6, 8]
+
