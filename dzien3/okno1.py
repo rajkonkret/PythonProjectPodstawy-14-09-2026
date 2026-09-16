697c85eb0109ceff424eacc37bdd2789c953053d
radek <rajkonkret660@gmail.com>
2026-09-16 15:02:03 +0200

    [3 33] update csv_zad1.py

---
+++ dzien3/okno1.py
@@ -0,0 +1,26 @@
+import tkinter
+
+
+class MyGui:
+    """
+    Okienko
+    """
+
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+        self.main_window.geometry("640x480")
+
+        self.label1 = tkinter.Label(self.main_window, text="Witaj Świecie")
+        self.label2 = tkinter.Label(self.main_window, text="Witaj Świecie")
+        self.label3 = tkinter.Label(self.main_window, text="Witaj Świecie")
+        self.label4 = tkinter.Label(self.main_window, text="Witaj Świecie")
+
+        self.label1.pack(side="left")
+        self.label2.pack(side="right")
+        self.label3.pack(side="top")
+        self.label4.pack(side="bottom")
+
+        tkinter.mainloop()
+
+
+my_gui = MyGui()
